2f6201ef0bebd5763751332d6d36d75a8922d662
Alexander Korshunov <71963376+akorshunov60@users.noreply.github.com>
2021-12-15 19:44:10 +0300

    file latest.tar содержит Docker образ приложения

---
+++ lesson8/frontend/src/app/student-form/student-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {StudentService} from "../service/student.service";
+import {Student} from "../service/student";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-student-form',
+  templateUrl: './student-form.component.html',
+  styleUrls: ['./student-form.component.less']
+})
+export class StudentFormComponent implements OnInit {
+
+  public student = new Student(null, "", "");
+
+  private mode: 'create' | 'edit' = 'create';
+
+  constructor(public studentService: StudentService,
+              public route: ActivatedRoute,
+              public router: Router) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(param => {
+      if (param.id) {
+        this.mode = 'edit';
+        this.studentService.findById(param.id)
+          .subscribe(student => {
+            this.student = student;
+          }, error => {
+            console.log(`Error ${error}`);
+          });
+      }
+    });
+  }
+
+  save() {
+    console.log(this.student);
+    if(this.mode == 'create') {
+      this.studentService.create(this.student).subscribe(() => this.onSave());
+    } else {
+      this.studentService.save(this.student).subscribe(() => this.onSave());
+    }
+  }
+
+  private onSave() {
+    this.router.navigateByUrl('/student');
+  }
+}
